247e8325164e66b8854339888e2b7b1c4afe2886
Ben Adams <thundercat@illyriad.co.uk>
2017-08-20 02:37:17 +0100

    Reduced MVC

--- _benchmarks/netcore-mvc/Program.cs
+++ _benchmarks/netcore-mvc/Program.cs
@@ -1,25 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace netcore_mvc
+public class Program
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
+    public static void Main(string[] args)
+        => WebHost.CreateDefaultBuilder(args)
+           .UseStartup<Program>().Build()
+           .Run();
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
-    }
+    public void ConfigureServices(IServiceCollection services)
+        => services.AddMvcCore();
+
+    public void Configure(IApplicationBuilder app)
+        => app.UseMvc();
 }
